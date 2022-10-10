8f1b4625aab4e22d9f39bf7dee52c8131cdd9ffc
Tenshi Kira <garciarodrigueza587@gmail.com>
2022-10-10 16:53:11 -0600

    Proyecto 1 colavo con Odin by T3nshi

---
+++ DoxDog.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import requests
+from colorama import Fore, Back, Style, init
+init()
+
+os.system("clear")
+def Doxxeo():
+    try:
+        
+        verde = Fore.GREEN
+        amar = Fore.YELLOW
+        rojo = Fore.RED
+        cyan = Fore.CYAN
+        magenta = Fore.MAGENTA
+
+        key = input(magenta + "Numero >> " + cyan)
+        print("")
+        url = f"https://doxdog-68f97-default-rtdb.firebaseio.com/numeros/{key}.json"
+
+        pedir = requests.get(url)
+        respuesta = json.loads(pedir.content)
+        
+        print(amar +"Numero: "+ verde + respuesta["num"])
+        print(amar +"Usuario: "+ verde + respuesta["nombre"])
+        print(amar +"Compañia: "+ verde + respuesta["compañia"])
+        print(amar +"Locacion: "+ verde + respuesta["locacion"])
+        Doxxeo()
+    except TypeError:
+        print(rojo + f"No se encuenta en la{verde} DataBase\n")
+        Doxxeo()
+
+Doxxeo()
